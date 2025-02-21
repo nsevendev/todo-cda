0e10e135d3f5fe96fb54c6683ba5b91866abaf95
jha <j.haimez@vattenfall.fr>
2025-02-21 22:07:51 +0100

    chore : start project

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+var environment = builder.Configuration["ASPNETCORE_ENVIRONMENT"] ?? "Non défini";
+var app = builder.Build();
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation("🚀 Démarrage de l'application");
+logger.LogInformation("🌿 Environnement détecté : {environment}", environment);
+
+app.MapGet("/", () => "Hello Worldssss!");
 
 app.Run();
