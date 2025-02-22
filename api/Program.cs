16a74880c73a739132df3377ecdb861ec6c86289
jha <j.haimez@vattenfall.fr>
2025-02-22 22:26:40 +0100

    chore : starter program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,20 @@
 using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
+using api;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables();
 var environment = builder.Configuration["ASPNETCORE_ENVIRONMENT"] ?? "Non défini";
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<TodoDatabaseContext>(options =>
+    options.UseNpgsql(connectionString)
+);
+
 var app = builder.Build();
 var logger = app.Services.GetRequiredService<ILogger<Program>>();
 
