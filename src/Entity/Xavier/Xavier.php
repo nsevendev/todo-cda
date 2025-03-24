bf759d8559667b5ce35d257627122de21eafe981
X4v_ <colombelxavier7@gmail.com>
2025-03-24 15:37:03 +0100

    Correction length lastname

--- src/Entity/Xavier/Xavier.php
+++ src/Entity/Xavier/Xavier.php
@@ -29,7 +29,7 @@ class Xavier
         return $this->firstname;
     }
 
-    #[ORM\Column(type: 'string', name: 'lastname', nullable: false, )]
+    #[ORM\Column(type: 'string', name: 'lastname', nullable: false, length: 25)]
     private string $lastname;
 
     public function lastname(): string
@@ -37,7 +37,7 @@ class Xavier
         return $this->lastname;
     }
 
-    #[ORM\Column(type: 'int', name: 'number', nullable: false, )]
+    #[ORM\Column(type: 'int', name: 'number', nullable: false)]
     private int $number;
 
     public function number(): int
