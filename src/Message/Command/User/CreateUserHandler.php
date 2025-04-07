76f959fe85f58edcc364d1a0748fc5e28d1b0c42
zmnllo <zimmermannmallorie29@gmail.com>
2025-04-07 15:56:38 +0200

    File correcting

--- src/Message/Command/User/CreateUserHandler.php
+++ src/Message/Command/User/CreateUserHandler.php
@@ -4,11 +4,46 @@ declare(strict_types=1);
 
 namespace Tocda\Message\Command\User;
 
-use Tocda\Entity\User\Dto\UserCreateDto;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Tocda\Entity\User\Dto\UserDto;
+use Tocda\Entity\User\User;
+use Tocda\Entity\User\ValueObject\UserEmail;
+use Tocda\Entity\User\ValueObject\UserUsername;
+use Tocda\Entity\User\ValueObject\UserPassword;
+use Tocda\Infrastructure\ApiResponse\Exception\Custom\Mercure\MercureInvalidArgumentException;
+use Tocda\Infrastructure\ApiResponse\Exception\Custom\User\UserInvalidArgumentException;   
+use Tocda\Infrastructure\Mercure\MercurePublish;
+use Tocda\Repository\User\UserRepository;
 
-class CreateUserCommand
+#[AsMessageHandler(bus: 'command.bus')]
+readonly class CreateUserHandler
 {
     public function __construct(
-        public UserCreateDto $userEntityCreateDto,
+        private UserRepository $userEntityRepository,
+        private MercurePublish $mercurePublish,
     ) {}
+
+    /**
+     * @throws MercureInvalidArgumentException
+     * @throws UserInvalidArgumentException
+     */
+    public function __invoke(CreateUserCommand $command): void
+    {
+        $user = new User(
+            username: UserUsername::fromValue($command->userEntityCreateDto->name),
+            email: UserEmail::fromValue($command->userEntityCreateDto->email),
+            password: UserPassword::fromValue($command->userEntityCreateDto->password)
+        );
+
+        $this->userEntityRepository->save( // Appel de la méthode save de la classe UserRepository
+            user: $user
+        );
+
+        $userDto = UserDto::fromArray($user);
+
+        $this->mercurePublish->publish(
+            topic: '/user-created',
+            data: $userDto->toArray()
+        );
+    }
 }
