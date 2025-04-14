a2b5367ad335f35cd42839d5c7bd17329692c4a2
zmnllo <zimmermannmallorie29@gmail.com>
2025-04-14 11:36:00 +0200

    Add UserUsernameTypeTest to entity User feat/entity-user-#5

---
+++ tests/Unit/Infrastructure/Doctrine/UserUsernameTypeTest.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tocda\Tests\Unit\Infrastructure\Doctrine;
+
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Type;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Tocda\Entity\User\ValueObject\UserUsername;
+use Tocda\Infrastructure\ApiResponse\Exception\Custom\AbstractApiResponseException;
+use Tocda\Infrastructure\ApiResponse\Exception\Custom\User\UserInvalidArgumentException;
+use Tocda\Infrastructure\ApiResponse\Exception\Error\Error;
+use Tocda\Infrastructure\Doctrine\Types\User\UserUsernameType;
+use Tocda\Tests\Unit\TocdaUnitTestCase;
+
+#[
+    CoversClass(UserUsernameType::class),
+    CoversClass(UserUsername::class),
+    CoversClass(AbstractApiResponseException::class),
+    CoversClass(UserInvalidArgumentException::class),
+    CoversClass(Error::class),
+]
+final class UserUsernameTypeTest extends TocdaUnitTestCase
+{
+    private UserUsernameType $type;
+    private MySQLPlatform $platform;
+
+    protected function setUp(): void
+    {
+        if (!Type::hasType('app_user_username')) {
+            Type::addType('app_user_username', UserUsernameType::class);
+        }
+
+        $this->type = Type::getType('app_user_username');
+        $this->platform = new MySQLPlatform();
+    }
+    public function testGetName(): void
+    {
+        self::assertSame('app_user_username', $this->type->getName());
+    }
+    public function testGetSQLDeclaration(): void
+    {
+        $column = ['length' => 255];
+        $sql = $this->type->getSQLDeclaration($column, $this->platform);
+        self::assertSame('VARCHAR(255)', $sql);
+    }
+    /**
+     * @throws UserInvalidArgumentException
+     * @throws ConversionException
+     */
+    public function testConvertToPHPValueWithValidString(): void
+    {
+        $userUsername = $this->type->convertToPHPValue('paquito', $this->platform);
+        self::assertInstanceOf(UserUsername::class, $userUsername);
+        self::assertSame('paquito', $userUsername->value());
+    }
+
+    /**
+     * @throws UserInvalidArgumentException
+     * @throws ConversionException
+     */
+    public function testConvertToPHPValueWithNull(): void
+    {
+        $userUsername = $this->type->convertToPHPValue(null, $this->platform);
+        self::assertNull($userUsername);
+    }
+    /**
+     * @throws UserInvalidArgumentException
+     * @throws ConversionException
+     */
+    public function testConvertToPHPValueWithInvalidType(): void
+    {
+        $this->expectException(UserInvalidArgumentException::class);
+        $this->type->convertToPHPValue(123, $this->platform);
+    }
+
+    /**
+     * @throws UserInvalidArgumentException
+     * @throws ConversionException
+     */
+    public function testConvertToDatabaseValueWithValidUserUsername(): void
+    {
+        $userUsername = new UserUsername('paquito');
+        $dbValue = $this->type->convertToDatabaseValue($userUsername, $this->platform);
+        self::assertSame('paquito', $dbValue);
+    }  
+
+    /**
+     * @throws UserInvalidArgumentException
+     * @throws ConversionException
+     */
+    public function testConvertToDatabaseValueWithNull(): void
+    {
+        $dbValue = $this->type->convertToDatabaseValue(null, $this->platform);
+        self::assertNull($dbValue);
+    }
+    /**
+     * @throws UserInvalidArgumentException
+     * @throws ConversionException
+     */
+    public function testConvertToDatabaseValueWithInvalidType(): void
+    {
+        $this->expectException(UserInvalidArgumentException::class);
+        $this->type->convertToDatabaseValue('Invalid Type', $this->platform);
+    }
+
+    public function testRequiresSQLCommentHint(): void
+    {
+        self::assertTrue($this->type->requiresSQLCommentHint($this->platform));
+    }
+}
